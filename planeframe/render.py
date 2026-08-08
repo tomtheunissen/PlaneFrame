d369182a070044201e523827209702e21a94fcf9
Tom Theunissen <tomtheunissen2006@gmail.com>
2026-08-09 01:01:57 +0200

    Create basic canvas with text

--- planeframe/render.py
+++ planeframe/render.py
@@ -1,8 +1,19 @@
 from PIL import Image, ImageDraw, ImageFont
+from pathlib import Path
 
 def create_canvas():
-    background = Image.new(mode = "RGB", size = (1200, 1600), color = (103, 190, 217))
-    background.show()
+    img = Image.new(mode = "RGB", size = (1200, 1600), color = (141, 223, 247))
+    return img
+
+
+def draw_text(img):
+    draw = ImageDraw.Draw(img)
+    font1 = ImageFont.truetype("assets/fonts/Inter_18pt-SemiBold.ttf", 90)
+    draw.text((10,10), "Airplane", font=font1, fill=(0, 0, 0))
+    return img
+
 
 if __name__ == "__main__":
-    create_canvas()
+    img = create_canvas()
+    img = draw_text(img)
+    img.show()
