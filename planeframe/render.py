d445ea5f785d65942a3de977bc83ade393a5db94
Tom Theunissen <tomtheunissen2006@gmail.com>
2026-08-09 19:30:27 +0200

    File paths now relative

--- planeframe/render.py
+++ planeframe/render.py
@@ -1,6 +1,10 @@
 from PIL import Image, ImageDraw, ImageFont
 from pathlib import Path
 
+ROOT = Path(__file__).resolve().parents[1]
+FONTS = ROOT / "assets" / "fonts"
+OUTPUT = ROOT / "output"
+
 def create_canvas():
     img = Image.new(mode = "RGB", size = (1200, 1600), color = (141, 223, 247))
     return img
@@ -8,7 +12,7 @@ def create_canvas():
 
 def draw_text(img):
     draw = ImageDraw.Draw(img)
-    font1 = ImageFont.truetype("assets/fonts/Inter_18pt-SemiBold.ttf", 90)
+    font1 = ImageFont.truetype(FONTS / "Inter_18pt-SemiBold.ttf",  90)
     draw.text((10,10), "Airplane", font=font1, fill=(0, 0, 0))
     return img
 
@@ -16,4 +20,5 @@ def draw_text(img):
 if __name__ == "__main__":
     img = create_canvas()
     img = draw_text(img)
-    img.show()
+    OUTPUT.mkdir(parents=True, exist_ok=True)
+    img.save(OUTPUT / "planes.png")
