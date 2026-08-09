e50bfb588b76e1fb1160c324f00b77546fc56b1a
Tom Theunissen <tomtheunissen2006@gmail.com>
2026-08-09 21:33:23 +0200

    Add planes on image

--- planeframe/render.py
+++ planeframe/render.py
@@ -15,7 +15,7 @@ def create_canvas():
 def draw_text(img, planes):
     draw = ImageDraw.Draw(img)
     font1 = ImageFont.truetype(SEMIBOLD, 55)
-    coord_y = 400
+    coord_y = 500
 
     for plane in planes:
         draw.text((50, coord_y), plane.description, font=font1, fill=(0, 0, 0))
@@ -23,10 +23,29 @@ def draw_text(img, planes):
     return img
 
 
+def draw_plane(img, planes):
+    draw = ImageDraw.Draw(img)
+    coord_y = 300
+
+    for plane in planes:
+        type_path = image_for(plane)
+        plane_img = Image.open(type_path).convert("RGBA")
+
+        w, h = plane_img.size
+        new_h = 200
+        new_w = int(w * (new_h / h))
+
+        plane_img = plane_img.resize((new_w, new_h))
+
+        img.paste(plane_img, (50, coord_y), plane_img)
+        coord_y = coord_y + 400
+    return img
+
 if __name__ == "__main__":
     from planeframe.models import aircraft_from_response
     from planeframe.sources.airplanes_live import load_sample
     from planeframe.filters import select_for_display
+    from planeframe.imagery import image_for
 
     result = load_sample("data/samples/20260808-163007.json")
     planes = aircraft_from_response(result)
@@ -34,5 +53,7 @@ if __name__ == "__main__":
 
     img = create_canvas()
     img = draw_text(img, planes)
+    img = draw_plane(img, planes)
+
     OUTPUT.mkdir(parents=True, exist_ok=True)
     img.save(OUTPUT / "planes.png")
